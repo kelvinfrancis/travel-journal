c3af01e7f9a54130bf7be8b7afe9cb003651af3b
kelvinfrancis <kelvinmoquete0597@gmail.com>
2025-09-09 13:40:37 -0400

    Refactor App component to use static data for travel entry and update Entry component to accept props for image and details

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
 import Entry from "./components/Entry";
 import Header from "./components/Header";
+import mountFuji from "./assets/mount-fuji.jpg";
 
 function App() {
-  const travels = [
-    {
-      title: "J A P A N",
-      locationTitle: "Mount Fuji",
-      locationLink: "",
-      date: "12 Jan, 2023 - 24 Jan, 2023",
-      details:
-        "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-    },
-  ];
   return (
     <>
       <Header />
       <main className="container">
-      {travels.map((obj) => (
         <Entry
-          key={obj.locationTitle}
-          title={obj.title}
-          location={obj.locationTitle}
-          link={obj.locationLink}
-          date={obj.date}
-          details={obj.details}
+          img={{
+            src: mountFuji,
+            alt: "Mount Fuji",
+          }}
+          title={"J A P A N"}
+          location={"Mount Fuji"}
+          link={"https://maps.app.goo.gl/urXtw6syDTkpj7LJ8"}
+          date={"12 Jan, 2023 - 24 Jan, 2023"}
+          details={"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."}
         />
-      ))}
       </main>
     </>
   );
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import markerIcon from "../assets/marker.svg";
-import mountFuji from "../assets/mount-fuji.jpg";
 
-export default function Entry({ title, location, link, date, details }) {
+export default function Entry(props) {
   return (
     <article className="journal-entry">
       <div className="image-container">
-        <img className="main-img" src={mountFuji} alt="entry-photo" />
+        <img className="main-img" src={props.img.src} alt={props.img.alt} />
       </div>
       <div className="info-container">
         <img className="marker" src={markerIcon} alt="marker-icon" />
-        <span className="country">{title}</span>
-        <a className="entry-location-link" href={link}>
+        <span className="country">{props.title}</span>
+        <a className="entry-location-link" target="_blank" href={props.link}>
           View on Google Maps
         </a>
-        <h2 className="entry-title">{location}</h2>
-        <span className="trip-date">{date}</span>
-        <p className="entry-text">{details}</p>
+        <h2 className="entry-title">{props.location}</h2>
+        <span className="trip-date">{props.date}</span>
+        <p className="entry-text">{props.details}</p>
       </div>
+      <hr />
     </article>
   );
 }
